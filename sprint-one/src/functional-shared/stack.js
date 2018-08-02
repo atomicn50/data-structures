1a10d1d657645692b9390774d7cbcac6c90c78c2
atomicn50 <39605644+atomicn50@users.noreply.github.com>
2018-08-01 20:36:05 -0700

    done

--- sprint-one/src/functional-shared/stack.js
+++ sprint-one/src/functional-shared/stack.js
@@ -1,8 +1,36 @@
 var Stack = function() {
-  // Hey! Rewrite in the new style. Your code will wind up looking very similar,
-  // but try not not reference your old code in writing the new style.
+  var someInstance = {};
+
+  var storage = {};
+
+  var count = 0;
+
+  someInstance.push = function(value) {
+    count++;  
+    storage[count] = value;
+
+  };
+
+  someInstance.pop = function() {
+    if (count > 0) {
+      count--;  
+    }
+
+    if (count === 0) {
+      var lastItem = storage[1];
+      delete storage[1];
+      return lastItem;
+    }
+    
+    var lastItem = storage[count + 1];
+    delete storage[count + 1];
+    return lastItem;
+         
+  };
+
+  someInstance.size = function() {
+    return count;
+  };
+
+  return someInstance;
 };
-
-var stackMethods = {};
-
-
