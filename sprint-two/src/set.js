4497b9b8a4585d2d52641f62d89c132916deced8
atomicn50 <tinhle46@gmail.com>
2018-08-03 16:11:13 -0700

    finished set

--- sprint-two/src/set.js
+++ sprint-two/src/set.js
@@ -1,18 +1,31 @@
 var Set = function() {
   var set = Object.create(setPrototype);
-  set._storage = null; // fix me
+  set._storage = {}; // fix me
   return set;
 };
 
 var setPrototype = {};
 
 setPrototype.add = function(item) {
+// input item
+// output none 
+// adding name to object
+  this._storage[item] = item;
 };
 
 setPrototype.contains = function(item) {
+// input name
+// output boolean if in object
+// if statment verifying 
+  if (this._storage[item]) {
+    return true;
+  } else {
+    return false;
+  }
 };
 
 setPrototype.remove = function(item) {
+  delete this._storage[item];
 };
 
 /*
