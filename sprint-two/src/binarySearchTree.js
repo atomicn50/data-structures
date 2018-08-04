af1e7c95358ff6b0811f812573f10671e653f44e
atomicn50 <tinhle46@gmail.com>
2018-08-04 12:12:45 -0700

    in progress, binaryTree, finished insert

--- sprint-two/src/binarySearchTree.js
+++ sprint-two/src/binarySearchTree.js
@@ -1,7 +1,48 @@
 var BinarySearchTree = function(value) {
+
+  var binaryTree = Object.create(binaryTreePrototype);
+  binaryTree.value = value; 
+
+  return binaryTree;
+};
+
+var binaryTreePrototype = {};
+
+
+binaryTreePrototype.insert = function (value) {
+// If value is less than parent value 
+  // If parent.left.value is undefined, instantiate a value
+  // Else, parent.left.insert (Recurse)
+
+// If value is greater than parent value 
+  // If parent.right.value is undefined, instantiate a value
+  // Else, parent.right.insert (Recurse)
+  if (value < this.value) {
+    if (this.left === undefined) {
+      this.left = BinarySearchTree(value);
+    } else {
+      this.left.insert(value);
+    }
+  }
+
+  if ( value > this.value) {
+    if (this.right === undefined) {
+      this.right = BinarySearchTree(value);
+    } else {
+      this.right.insert(value);
+    }
+  }
+console.log(this)
+};
+
+binaryTreePrototype.contains = function (value) {
+};
+
+binaryTreePrototype.depthFirstLog = function (cb) {
 };
 
 
 /*
  * Complexity: What is the time complexity of the above functions?
  */
+
